c881a441da6989fa784461d3434dcec2bec398ee
Hao Ho-Minh <minhhaovh11@gmail.com>
2024-09-24 14:31:56 +0700

    Added setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Maths chatbot project",
+    version = "0.0.1",
+    author = "Hao Ho-Minh",
+    author_email = "minhhaovh11@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
